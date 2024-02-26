f9b8151593cc398e34b6aab261705802c6f0e119
Anshika <145582622+AnshikaDabodia@users.noreply.github.com>
2024-02-26 23:55:19 +0530

    chatbot ai

--- CodeAlpha_Aichatbot.cpp
+++ CodeAlpha_Aichatbot.cpp
@@ -4,7 +4,6 @@
 #include <map>
 
 std::string preprocessInput(const std::string& input) {
-
     std::string processedInput = input;
     std::transform(processedInput.begin(), processedInput.end(), processedInput.begin(), ::tolower);
     return processedInput;
